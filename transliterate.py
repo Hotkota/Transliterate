e99b2fdd3b9d504eeda865620d4f29d9c5740d29
Hotkota <kisamawk@gmail.com>
2020-10-17 23:47:28 +0700

    New lang

--- transliterate.py
+++ transliterate.py
@@ -4,27 +4,30 @@ import importlib
 
 class Transliterate:
 	def __init__(self, text, language_input = None, language_output = None):
-		self.text = text.lower()
+		self.text = text
 		if language_output is None:
 			raise ValueError("Укажите выходной язык")
 		elif language_input is None:
 			raise ValueError("Укажите язык оригинала")
 		else:
-			if language_output.isalpha() and language_input.isalpha():
-				if len(language_output) == 2 and len(language_input) == 2: 
-					for i in glob.glob("./language/*"):
-						if i[-2:] == language_output:
-							self.language_output = language_output.lower()
-							break
+			if language_output.lower().isalpha() and language_input.lower().isalpha():
+				if len(language_output) == 2 and len(language_input) == 2:
+					if language_output.lower() == language_input.lower():
+						raise ValueError("Выходной и оригинальный язык не могут быть одинаковыми")
 					else:
-						raise ValueError("Укажите верный выходящий язык")
+						for i in glob.glob("./language/*"):
+							if i[-2:] == language_output.lower():
+								self.language_output = language_output.lower()
+								break
+						else:
+							raise ValueError("Укажите верный выходящий язык")
 
-					for i in glob.glob("./language/*"):
-						if i[-2:] == language_input:
-							self.language_input = language_input
-							break
-					else:
-						raise ValueError("Укажите верный язык оригинала")
+						for i in glob.glob("./language/*"):
+							if i[-2:] == language_input.lower():
+								self.language_input = language_input.lower()
+								break
+						else:
+							raise ValueError("Укажите верный язык оригинала")
 				else:
 					raise ValueError("Длина языка не может привышать 2 символа")
 			else:
@@ -35,11 +38,11 @@ class Transliterate:
 			for l in glob.glob(i + "\\*"):
 				if os.path.basename(l).endswith(".py") and os.path.basename(l).startswith("litters"):
 					file = importlib.import_module(l.replace("\\", ".")[2:-3])
-
+	
 		for key in file.litters:
 			self.text = self.text.replace(key, file.litters[key])
-
+	
 		return self.text
 
-iam = Transliterate("privet", language_input = "en", language_output = "ru")
-iam.translite()
+iam = Transliterate("привет", language_input = "ru", language_output = "ja")
+print(iam.translite())
